66ca717e42f32c2d0fc1964c482ba867f5bc554d
NoiseyLobster <noisey.lobster@gmail.com>
2024-01-12 16:00:36 -0800

    condensed file structure to reduce repeat code

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express')
 const server = express()
 const port = 8080
 
-//make resources avaliable in the public directory for download to the browser 
-server.use(express.static('public'));
+//make resources avaliable in the public directories for download to the browser 
+server.use(express.static('fileHostingForOBS'));
 
 //HTTP GET
 //Routing for the base url
 server.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/panel.html");
+    res.sendFile(__dirname + "/panelExtension/panel.html");
 });
 
 server.get('/config.html', (req, res) => {
-    res.sendFile(__dirname + "/public/panel.html");
+    res.sendFile(__dirname + "/panelExtension/panel.html");
 })
 
 server.get('/panel.html', (req, res) => {
-    res.sendFile(__dirname + "/public/panel.html");
+    res.sendFile(__dirname + "/panelExtension/panel.html");
 })
 
 server.listen(port, () => {
