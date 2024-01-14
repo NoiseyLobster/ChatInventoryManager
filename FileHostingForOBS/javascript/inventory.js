1d2c1bba6a5422b89c745d4db79280ee9398337d
NoiseyLobster <noisey.lobster@gmail.com>
2024-01-13 23:06:39 -0800

    command suppot for adding new item types

--- FileHostingForOBS/javascript/inventory.js
+++ FileHostingForOBS/javascript/inventory.js
@@ -2,7 +2,8 @@ const chatCommands = {
     commandPrefix: "!",
     addToInventory: "!add",
     removeFromInventory: "!del",
-    addItemType: "!newItem",
+    newPotionType: "!addPotionType",
+    newBattleItemType: "!addBattleItemType",
     removeItemType: "!removeItem"
 }
 
@@ -19,24 +20,30 @@ const inventoryManager = {
             //if (user['username'] === "noiseylobster13") {
             if ((user['username'] === vote.channel || user.mod)) {
                 if (message.toLowerCase().startsWith(chatCommands.commandPrefix)) {
-                    //extract the portion in quotes that should have our item and then remove the quotes
+                    //extract the command
+                    var command = message.split(' ')[0];
+
+                    //extract the portion in quotes that should have our item (includes quotes)
                     var itemNameInQuotes = message.match(/"(.*?)"/);
+                    var itemName = itemNameInQuotes != null ? itemNameInQuotes[1].replace(/["]/g, "").trim() : null;
 
-                    if (itemNameInQuotes != null) {
-                        var itemName = itemNameInQuotes[1].replace(/["]/g, "").trim();
+                    //if a quantity is provided extract that as well
+                    var quantityString = message.match(/\d+$/);
+                    var quantity = quantityString != null ? parseInt(quantityString[0]) : null;
 
-                        //extract the quantity number from the end of the command
-                        var quantity = message.match(/\d+$/);
-                        if (quantity != null) {
-                            var quantityAsInt = parseInt(quantity[0]);
-
-                            if (message.toLowerCase().startsWith(chatCommands.addToInventory)) {
-                                inventoryManager.addItemToInventory(itemName, quantityAsInt);
-                            }
-                            else if (message.toLowerCase().startsWith(chatCommands.removeFromInventory)) {
-                                inventoryManager.removeItemFromInventory(itemName, quantityAsInt);
-                            }
-                        }
+                    switch (command) {
+                        case chatCommands.addToInventory:
+                            inventoryManager.addItemToInventory(itemName, quantity);
+                            break;
+                        case chatCommands.removeFromInventory:
+                            inventoryManager.removeItemFromInventory(itemName, quantity);
+                            break;
+                        case chatCommands.newPotionType:
+                            inventoryManager.createNewPotionType(itemName, quantity);
+                            break;
+                        case chatCommands.newBattleItemType:
+                            inventoryManager.createNewBattleItemType(itemName, quantity);
+                            break;
                     }
                 }
                 else if (user['username'].toLowerCase() === "streamlootsbot") {
@@ -63,11 +70,11 @@ const inventoryManager = {
         "Blister Berry",
         "Swamp Glider Legs",
         "Nethergill Puffcap",
-        "Moss Covered Pebble",        
+        "Moss Covered Pebble",
         "Blightsurge Fruit",
         "Mirebind Creeper Vine"
     ],
-    uncategorizedItems: [  
+    uncategorizedItems: [
         "Potion of Hubris",
         "Golden Crested Finch Song",
         "Whispering Wind Crystal",
@@ -79,7 +86,7 @@ const inventoryManager = {
         "Liminal Halite",
         "Nibiru Leaf"
     ],
-    getAllMagicItems: function() { return inventoryManager.potions.concat(inventoryManager.battleItems, inventoryManager.uncategorizedItems); },
+    getAllMagicItems: function () { return inventoryManager.potions.concat(inventoryManager.battleItems, inventoryManager.uncategorizedItems); },
     inventory: [],
     itemTemplate: ({ title, amount }) => `
     <div class="row inventory-item" id="${title.toLowerCase().replace(/ /g, "-")}">
@@ -87,54 +94,76 @@ const inventoryManager = {
     </div>
     `,
     addItemToInventory: function (itemName, quantityToAdd = 1) {
-        var itemIndex = inventoryManager.getAllMagicItems().indexOf(itemName);
-        var formattedItemName = itemName.toLowerCase().replace(/ /g, "-");
-
-        if (itemIndex != -1) {
-            if ($(".inventory-item").is("#" + formattedItemName)) {  //we already have at least one of these in inventory
-                inventoryManager.inventory[itemIndex] += quantityToAdd;
-
-                $("#" + formattedItemName).replaceWith([
-                    { title: itemName, amount: inventoryManager.inventory[itemIndex] }
-                ].map(inventoryManager.itemTemplate).join(''));
+        if(itemName != null) {
+            var itemIndex = inventoryManager.getAllMagicItems().indexOf(itemName);
+            var formattedItemName = itemName.toLowerCase().replace(/ /g, "-");
+            quantityToAdd = quantityToAdd ?? 1;
+    
+            if (itemIndex != -1) {
+                if ($(".inventory-item").is("#" + formattedItemName)) {  //we already have at least one of these in inventory
+                    inventoryManager.inventory[itemIndex] += quantityToAdd;
+    
+                    $("#" + formattedItemName).replaceWith([
+                        { title: itemName, amount: inventoryManager.inventory[itemIndex] }
+                    ].map(inventoryManager.itemTemplate).join(''));
+                }
+                else {                                                   //we dont have any in inventory yet
+                    inventoryManager.inventory[itemIndex] = quantityToAdd;
+    
+                    var categoryID = inventoryManager.potions.includes(itemName) ? "#potion-inventory" : "#battle-item-inventory";
+    
+                    $("#inventory " + categoryID).append([
+                        { title: itemName, amount: inventoryManager.inventory[itemIndex] }
+                    ].map(inventoryManager.itemTemplate).join(''));
+                }
             }
-            else {                                                   //we dont have any in inventory yet
-                inventoryManager.inventory[itemIndex] = quantityToAdd;
-
-                var categoryID = inventoryManager.potions.includes(itemName) ? "#potion-inventory" : "#battle-item-inventory";
-
-                $("#inventory " + categoryID).append([
-                    { title: itemName, amount: inventoryManager.inventory[itemIndex] }
-                ].map(inventoryManager.itemTemplate).join(''));
+            else {
+                console.log("Not among allowed inventory items");
             }
-        }
-        else {
-            console.log("Not among allowed inventory items");
         }
 
         console.log(inventoryManager.inventory);
     },
     removeItemFromInventory: function (itemName, quantityToRemove = 1) {
-        var itemIndex = inventoryManager.getAllMagicItems().indexOf(itemName);
-        var formattedItemName = itemName.toLowerCase().replace(/ /g, "-");
-
-        if (itemIndex != -1 && inventoryManager.inventory[itemIndex] >= 0) {
-            inventoryManager.inventory[itemIndex] = Math.max(inventoryManager.inventory[itemIndex] - quantityToRemove, 0);
-
-            if (inventoryManager.inventory[itemIndex] > 0) {
-                $("#" + formattedItemName).replaceWith([
-                    { title: itemName, amount: inventoryManager.inventory[itemIndex] }
-                ].map(inventoryManager.itemTemplate).join(''));
+        if(itemName != null) {
+            var itemIndex = inventoryManager.getAllMagicItems().indexOf(itemName);
+            var formattedItemName = itemName.toLowerCase().replace(/ /g, "-");
+            quantityToRemove = quantityToRemove ?? 1;
+    
+            if (itemIndex != -1 && inventoryManager.inventory[itemIndex] >= 0) {
+                inventoryManager.inventory[itemIndex] = Math.max(inventoryManager.inventory[itemIndex] - quantityToRemove, 0);
+    
+                if (inventoryManager.inventory[itemIndex] > 0) {
+                    $("#" + formattedItemName).replaceWith([
+                        { title: itemName, amount: inventoryManager.inventory[itemIndex] }
+                    ].map(inventoryManager.itemTemplate).join(''));
+                }
+                else {
+                    $("#" + formattedItemName).remove();
+                }
             }
             else {
-                $("#" + formattedItemName).remove();
+                console.log("Not among allowed inventory items");
             }
-        }
-        else {
-            console.log("Not among allowed inventory items");
         }
 
         console.log(inventoryManager.inventory);
+    },
+    createNewPotionType: function (itemName, initialAmount = 0) {
+        initialAmount = initialAmount ?? 0;
+
+        inventoryManager.potions.push(itemName);
+        if(initialAmount > 0) {
+            inventoryManager.addItemToInventory(itemName, initialAmount);
+        }
+    },
+    createNewBattleItemType: function (itemName, initialAmount = 0) {
+        initialAmount = initialAmount ?? 0;
+
+        inventoryManager.battleItems.push(itemName);
+        if(initialAmount > 0) {
+            inventoryManager.addItemToInventory(itemName, initialAmount);
+        }        
     },
     client: {},
     channel: window.location.hash.slice(1).toLowerCase()
